2a4c1c544e042db2ecf23582a115b3499086987a
Nilson Santos <python.nilson@gmail.com>
2023-08-18 19:49:33 -0300

    atualizacao exercicio

--- Curso em Video (canal do Youtube)/5. Manipulando Texto/023 - Separando digitos de um numero/separando_digitos_de_um_numero_23.py
+++ Curso em Video (canal do Youtube)/5. Manipulando Texto/023 - Separando digitos de um numero/separando_digitos_de_um_numero_23.py
@@ -3,10 +3,10 @@ Programa que lê um número de 0 a 9999 e exibe na tela o
 milhar, a centena, dezena e a unidade referente a esse número.
 """
 
-from math import floor
-
-numero = int(input('Digite um número de 0 a 9999: '))
-print(f'Unidade: {numero-(floor(numero/10)*10)}')
-print(f'Dezena: {floor((numero-(floor(numero/100)*100))/10)}')
-print(f'Centena: {floor((numero-floor(numero/1000)*1000)/100)}')
-print(f'Milhar: {floor(numero/1000)}')
+NUMERO = str(input('Digite um número de 0 a 9999: '))
+NUMERO = '-0-'.join('00' + NUMERO).split('-')
+TAMANHO_STRING = len(NUMERO)
+print(f'Unidade: {NUMERO[TAMANHO_STRING - 1]}')
+print(f'Dezena: {NUMERO[TAMANHO_STRING - 3]}')
+print(f'Centena: {NUMERO[TAMANHO_STRING - 5]}')
+print(f'Milhar: {NUMERO[TAMANHO_STRING - 7]}')
