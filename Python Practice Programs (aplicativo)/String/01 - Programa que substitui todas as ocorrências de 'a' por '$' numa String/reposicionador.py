d44201278e49616359c1b546bb5ed6aeec236bef
Nilson Santos <python.nilson@gmail.com>
2023-08-25 23:36:51 -0300

    atualizacao de exercicio

--- Python Practice Programs (aplicativo)/String/01 - Programa que substitui todas as ocorrências de 'a' por '$' numa String/reposicionador.py
+++ Python Practice Programs (aplicativo)/String/01 - Programa que substitui todas as ocorrências de 'a' por '$' numa String/reposicionador.py
@@ -3,7 +3,7 @@ Programa que lê uma string qualquer e substitui todas as ocorrências de 'a',
 seja em letra minúscula ou maiúscula, por "$".
 """
 
-string = input('Digite uma String: ')
+string = input('\033[1:32mDigite uma String: \033[m')
 string = string.replace("a", "$")
 string = string.replace("A", "$")
-print(f'String modificada: {string}')
+print(f'\033[1:32mString modificada: \033[1:31m{string}')
